711fa26cbfee549c8079fe5e5366b6e43140ccb4
Derek Mathieson <derek.mathieson@gmail.com>
2026-05-14 15:28:31 +0200

    new build - working.

--- include/BuildMetadata.h
+++ include/BuildMetadata.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #define APP_BUILD_VERSION "1.0.0"
-#define APP_BUILD_GIT_SHA "c205a2a"
-#define APP_BUILD_GIT_REF "c205a2a-dirty"
-#define APP_BUILD_ENV "nodemcu-32s_relay"
-#define APP_BUILD_TIME_UTC "2026-05-12T12:55:04Z"
-#define APP_BUILD_SUMMARY "1.0.0 (c205a2a)"
+#define APP_BUILD_GIT_SHA "deab257"
+#define APP_BUILD_GIT_REF "deab257-dirty"
+#define APP_BUILD_ENV "nodemcu-32s"
+#define APP_BUILD_TIME_UTC "2026-05-13T20:35:24Z"
+#define APP_BUILD_SUMMARY "1.0.0 (deab257)"
